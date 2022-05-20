1a00bf484aa5d81a75bf4d891f1f41ef37ccd945
张云鹏 <370397727@qq.com>
2022-05-20 10:05:11 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+    msg: String
+})
+
+let count = ref(0)
+
+let btn = () => {
+    count.value++
+}
+
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <button type="button" @click="btn">count is: {{ count }}</button>
+
+</template>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
